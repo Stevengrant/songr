f93b4ce7b4d687014e89db99d5e9b56994addce3
Steven Grant <blue.ninjaneer@gmail.com>
2019-08-20 13:46:26 -0700

    updated reverse

--- src/main/java/com/fakesitewoopasdf/songr/Helpers.java
+++ src/main/java/com/fakesitewoopasdf/songr/Helpers.java
@@ -4,13 +4,14 @@ public class Helpers {
     public Helpers(){
     }
     public String reverseString ( String string) {
-        String[] splitSting = string.split("");
+        String[] splitSting = string.split(" ");
         String stringAcc = "";
 
         for (int i = splitSting.length - 1; i > -1 ; i--){
 
-            stringAcc+=splitSting[i];
+            stringAcc+=splitSting[i] + " ";
         }
+
         return stringAcc;
     }
 }
