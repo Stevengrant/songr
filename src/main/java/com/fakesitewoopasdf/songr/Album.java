a55dfc5dfa55afba2fd5773ae48d413944013944
Steven Grant <blue.ninjaneer@gmail.com>
2019-08-21 13:34:19 -0700

    boiler plate

--- src/main/java/com/fakesitewoopasdf/songr/Album.java
+++ src/main/java/com/fakesitewoopasdf/songr/Album.java
@@ -1,4 +1,27 @@
 package com.fakesitewoopasdf.songr;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
 public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String title;
+    String artist;
+    int songCount;
+    int lengthInSeconds;
+    String albumArtUrl;
+    public Album(){};
+    public Album(String title, String artist, int songCount, int lengthInSeconds, String albumArtUrl){
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.lengthInSeconds = lengthInSeconds;
+        this.albumArtUrl = albumArtUrl;
+    }
 }
---
+++ src/main/java/com/fakesitewoopasdf/songr/AlbumRepository.java
@@ -0,0 +1,7 @@
+package com.fakesitewoopasdf.songr;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlbumRepository extends JpaRepository<Album, Long> {
+
+}
