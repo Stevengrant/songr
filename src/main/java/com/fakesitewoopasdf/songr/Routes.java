9ee62fa8e067aafae8d1f6f1cf57427c567ec48e
Steven Grant <blue.ninjaneer@gmail.com>
2019-08-21 13:45:44 -0700

    making progress: added html page and finished up w/ db boiler plate

--- src/main/java/com/fakesitewoopasdf/songr/Routes.java
+++ src/main/java/com/fakesitewoopasdf/songr/Routes.java
@@ -1,12 +1,26 @@
 package com.fakesitewoopasdf.songr;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
 @Controller
 public class Routes {
+
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @GetMapping("/albums")
+    public String getAllAlbums(Model m){
+        List<Album> albums = albumRepository.findAll();
+        m.addAttribute("Albums",albums);
+        return "albums";
+    }
     @GetMapping("/hello")
     public String helloWorld() {
         System.out.println("somebody got the root");
